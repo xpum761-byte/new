759c0f1ea89b13a690258bde806d10fc1bfd8d4b
xpum761-byte <xpum761@gmail.com>
2025-09-26 01:34:12 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
-      plugins: [react()],
-      resolve: {
-        alias: {
-          // Fix: In ES modules, __dirname is not available. Use import.meta.url instead.
-          '@': fileURLToPath(new URL('.', import.meta.url)),
-        }
-      }
+      plugins: [react()]
     };
 });
