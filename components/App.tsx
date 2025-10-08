0f95977dbea840c92bd2753863234e49c5b33f9f
xpum761-byte <xpum761@gmail.com>
2025-10-08 22:46:53 +0700

    Update App.tsx

--- components/App.tsx
+++ components/App.tsx
@@ -196,7 +196,7 @@ const App: React.FC = () => {
                 aspectRatio: string;
             };
         } = {
-            model: 'veo-2.0-generate-001',
+            model: 'veo-3.0-fast-generate-001',
             prompt: finalPrompt.trim().replace(/\s\s+/g, ' '),
             config: { 
                 numberOfVideos: 1,
@@ -310,7 +310,7 @@ const App: React.FC = () => {
                             aspectRatio: string;
                         };
                     } = {
-                        model: 'veo-2.0-generate-001',
+                        model: 'veo-3.0-fast-generate-001',
                         prompt: finalPrompt.trim().replace(/\s\s+/g, ' '),
                         config: { 
                             numberOfVideos: 1,
